1ae5c5af4b72ffb74c57f60276bf570569f02f58
oskvorcova <130305258+oskvorcova@users.noreply.github.com>
2023-06-09 19:40:51 +0300

    Update universities.sql

--- sql/universities.sql
+++ sql/universities.sql
@@ -6,15 +6,15 @@ INTO
         country,
         date_st)
 VALUES
-    ("Harvard university","Гарвардский университет", 'England', "2022-09-01"),
-    ("Bocconi university","Университет боккони", 'Italy', "2022-09-01"),
-    ("LSE London","Лондонская школа экономики и политических наук", 'England', "2022-09-01"),
-    ("Science university Paris","Парижский университет", 'France', "2022-06-01"),
-    ("MIT","массачусетский технологический институт", 'USA', "2022-09-01"),
-    ("Stanford university","Университет Стэнфорд", 'USA', "2022-09-01"),
-    ("WU Vienna","Венский университет", 'Austria', "2022-07-01"),
-    ("Moscow State university","МГУ", 'Russia', "2022-07-01"),
-    ("mipt Russia","МФТИ", 'Russia', "2022-06-01"),
-    ("National Research Nuclear University Russia","МИФИ", 'Russia', "2022-06-01"),
-    ("Higher School of Economics Russia","вшэ", 'Russia', "2022-06-01"),
-    ("Erasmus university Rotterdam","Университет Эрасмус", 'Netherland', "2022-09-01");
+    ("Harvard university","Гарвардский университет", 'USA', "2022-09-01", "2023-01-01"),
+    ("Bocconi university","Университет Боккони", 'Italy', "2023-02-01", "2023-04-19"),
+    ("LSE","Лондонская школа экономики и политических наук", 'England', "2022-09-01", "2023-01-25"),
+    ("SciencesPo","Парижский институт политических исследований", 'France', "2022-10-01", "2023-02-26"),
+    ("MIT","Массачусетский технологический институт", 'USA', "2022-09-01", "2023-01-05"),
+    ("Stanford university","Стэнфордский университет", 'USA', "2022-09-15", "2023-01-05"),
+    ("WU Vienna","Венский университет Экономики и Бизнеса", 'Austria', "2022-09-01", "2023-10-08"),
+    ("Erasmus university Rotterdam","Университет Эразма Роттердамского", 'Netherland', "2022-09-01"),
+    ("Moscow State university","МГУ", 'Russia', "2022-06-20", "2022-07-10"),
+    ("MIPT","МФТИ", 'Russia', "2022-06-20", "2022-08-05"),
+    ("National Research Nuclear University"," НИЯУ МИФИ", 'Russia', "2022-06-15", "2022-07-25"),
+    ("HSE University","НИУ ВШЭ", 'Russia', "2022-06-20", "2022-07-25");
